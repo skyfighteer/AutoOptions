f58c5916038aaba65f1d30367710deff6d9a13ef
skyfighteer <50503537+skyfighteer@users.noreply.github.com>
2022-10-27 17:10:28 +0200

    Update background.js

--- AutoOptions Example Extension/background.js
+++ AutoOptions Example Extension/background.js
@@ -1,13 +1,5 @@
-let optionsPage;
 chrome.runtime.onInstalled.addListener((details) => {
-    const options = chrome.runtime.getManifest().options_ui;
-    const popup = chrome.runtime.getManifest().action;
-    if (options) optionsPage = options.page;
-    else if (popup) optionsPage = popup.default_popup;
-    else throw new Error("AutoOptions Error: There is no options page declared in the Manifest file.");
-    if (details.reason === "install") {
-        createOptionsTab();
-    };
+    if (details.reason === "install") createOptionsTab();
 });
 
 chrome.runtime.onMessage.addListener((message)=> {
@@ -15,6 +7,12 @@ chrome.runtime.onMessage.addListener((message)=> {
 });
 
 function createOptionsTab() {
+    let optionsPage;
+    const options = chrome.runtime.getManifest().options_ui;
+    const popup = chrome.runtime.getManifest().action;
+    if (options) optionsPage = options.page;
+    else if (popup) optionsPage = popup.default_popup;
+    else throw new Error("AutoOptions Error: There is no options page declared in the Manifest file.");
     chrome.tabs.create({
         url: optionsPage
     });
